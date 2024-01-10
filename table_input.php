46a191911a558d3ff68b3cdb176a9b09d88db075
DharmiGandecha <154863984+DharmiGandecha@users.noreply.github.com>
2024-01-10 18:11:29 +0530

    Add files via upload

---
+++ table_input.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<body>
+<form method="get" action="multitab.php">
+enter number for start:<input type="text" name="sno"><br>
+enter number for end:<input type="text" name="lno"><br>
+<input type="submit" name="submit">
+</form>
+
+</body>
+</head>
+</html>
