152cf9ffabe31283c231b592481778f9ea273373
DharmiGandecha <154863984+DharmiGandecha@users.noreply.github.com>
2024-02-16 07:48:24 +0530

    Add files via upload

---
+++ clock.php
@@ -0,0 +1,38 @@
+
+<html>
+
+<meta http-equiv="refresh" content="1">
+
+
+     <style>
+        .a{
+            
+            color:blue;
+            font-size:50px;
+			background-color:black;
+			border-color:grey;
+		
+			
+			font-family:digital dream fat;
+			src:url(digital-dream\digital dream fat);
+			
+        }
+    </style>
+
+
+  <body>
+<div>  
+<?php
+echo "<table colspan=2 border=4 class=a>";
+echo "<tr>";
+echo "<td>";
+date_default_timezone_set("Asia/kolkata");
+echo date("h:i:s a");
+echo "</td>";
+echo "</tr>";
+echo "</table>";
+?>
+</div>
+</body>
+
+</html>
